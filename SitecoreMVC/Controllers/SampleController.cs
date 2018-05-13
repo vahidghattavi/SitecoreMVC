720a582eb98a88710323ccd50a23f6e3f2f35565
Vahid Ghattavi <vahid.ghattavi@rxpservices.com>
2018-05-13 23:28:13 +1000

    Initial Commit

---
+++ SitecoreMVC/Controllers/SampleController.cs
@@ -0,0 +1,20 @@
+﻿using Glass.Mapper.Sc.Web.Mvc;
+using SitecoreMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SitecoreMVC.Controllers
+{
+    public class SampleController : GlassController
+    {
+        // GET: Sample
+        public ActionResult Listing()
+        {
+            var context = SitecoreContext.GetCurrentItem<IGlassBase>();
+            return View("TestView");
+        }
+    }
+}
---
+++ SitecoreMVC/Models/IGlassBase.cs
@@ -0,0 +1,18 @@
+﻿using Glass.Mapper.Sc.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SitecoreMVC.Models
+{
+    public interface IGlassBase
+    {
+        [SitecoreInfo(Glass.Mapper.Sc.Configuration.SitecoreInfoType.Name)]
+        string Name { get; set; }
+
+        [SitecoreInfo(Glass.Mapper.Sc.Configuration.SitecoreInfoType.TemplateName)]
+        string TemplateName { get; set; }
+    }
+}
