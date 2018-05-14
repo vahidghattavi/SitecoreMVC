116b3b3a9896e634ae350977a3f46bc71345c9a3
Vahid Ghattavi <vahid.ghattavi@rxpservices.com>
2018-05-14 12:53:38 +1000

    Added Unit Test Project

---
+++ SitecoreMVC.Test/ControllerUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SitecoreMVC.Test
+{
+    [TestClass]
+    public class ControllerUnitTest
+    {
+        [TestMethod]
+        public void SampleUnitTest()
+        {
+            var a = 2;
+            var b = 3;
+            Assert.AreEqual(a + b, 5);
+        }
+    }
+}
---
+++ SitecoreMVC.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SitecoreMVC.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SitecoreMVC.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("280b439f-7f4f-42b3-a0d1-758c1a46a298")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
